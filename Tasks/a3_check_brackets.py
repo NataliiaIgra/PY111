ab1e1a38f721a83f281ad284300c369bec556bbf
NataliiaIgra <natasha_igra@mail.ru>
2020-12-25 13:14:14 +0300

    Реализация проверки скобочной последовательности.

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -5,5 +5,15 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
-
-    return False
+    skobochka = 0
+    for bracket in brackets_row:
+        if bracket == "(":
+            skobochka += 1
+        else:
+            skobochka -= 1
+        if skobochka < 0:
+            return False
+    if skobochka == 0:
+        return True
+    else:
+        return False
