bfb89cf0f1321a5c6494f588e3844e9b14ba2c9b
NataliiaIgra <natasha_igra@mail.ru>
2020-12-26 12:33:13 +0300

    Реализация линейного поиска.

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -4,6 +4,24 @@ This module implements some functions based on linear search algo
 from typing import Sequence
 
 
+def search(arr: Sequence, elem: int) -> int:
+    """
+    Function that finds required element in array
+
+    :param arr: Array containing numbers
+    :param elem: Number to find in the array
+    :return: index of first occurrence of required element in array
+    """
+    if not arr:
+        return -1
+    for index, element in enumerate(arr):
+        if element == elem:
+            return index
+        else:
+            continue
+    return -1
+
+
 def min_search(arr: Sequence) -> int:
     """
     Function that find minimal element in array
@@ -11,5 +29,12 @@ def min_search(arr: Sequence) -> int:
     :param arr: Array containing numbers
     :return: index of first occurrence of minimal element in array
     """
-    print(arr)
-    return -1
+    if not arr:
+        return -1
+    min_index = 0
+    min_element = arr[0]
+    for index, element in enumerate(arr):
+        if element < min_element:
+            min_element = element
+            min_index = index
+    return min_index
