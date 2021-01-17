5a859f00ff17855931daeaedc59774e52415c731
NataliiaIgra <natasha_igra@mail.ru>
2021-01-17 14:31:15 +0300

    Двоичное дерево поиска.

--- Tasks/f0_binary_search_tree.py
+++ Tasks/f0_binary_search_tree.py
@@ -4,8 +4,11 @@ or with dicts (smth like {'key': 0, value: 123, 'left': {...}, 'right':{...}})
 """
 
 from typing import Any, Optional, Tuple
-# import networkx as nx
+import networkx as nx
+import matplotlib.pyplot as plt
 
+Tree = nx.DiGraph()
+root = None
 
 def insert(key: int, value: Any) -> None:
     """
@@ -15,7 +18,43 @@ def insert(key: int, value: Any) -> None:
     :param value: value associated with key
     :return: None
     """
-    print(key, value)
+    global Tree, root
+    if not Tree.nodes:
+        Tree.add_node(key, data=value)
+        root = key
+    else:
+        current_node = root
+        search = True
+        while search:
+            if key == current_node:
+                Tree.nodes[key]['data'] = value
+                search = False
+            else:
+                neighbours = list(Tree.neighbors(current_node))
+                if not neighbours:
+                    Tree.add_node(key, data=value)
+                    Tree.add_edge(current_node, key)
+                    search = False
+                elif len(neighbours) == 1:
+                    if neighbours[0] > current_node:
+                        if key > current_node:
+                            current_node = neighbours[0]
+                        else:
+                            Tree.add_node(key, data=value)
+                            Tree.add_edge(current_node, key)
+                            search = False
+                    else:
+                        if key > current_node:
+                            Tree.add_node(key, data=value)
+                            Tree.add_edge(current_node, key)
+                            search = False
+                        else:
+                            current_node = neighbours[0]
+                else:
+                    if key > current_node:
+                        current_node = max(neighbours)
+                    else:
+                        current_node = min(neighbours)
     return None
 
 
@@ -26,6 +65,17 @@ def remove(key: int) -> Optional[Tuple[int, Any]]:
     :param key: key to be removed
     :return: deleted (key, value) pair or None
     """
+    global Tree, root
+    parent_node = root
+    current_node = root
+    search = True
+    while search:
+        if key == current_node:
+            neighbours = list(Tree.neighbors(current_node))
+            if not neighbours:
+                Tree.remove_node(current_node)
+            search = False
+
     print(key)
     return None
 
@@ -37,8 +87,12 @@ def find(key: int) -> Optional[Any]:
     :param key: key for search in the BST
     :return: value associated with the corresponding key
     """
-    print(key)
-    return None
+    global Tree
+    exist_nodes = list(Tree.nodes)
+    if key in exist_nodes:
+        return Tree.nodes[key]['data']
+    else:
+        raise KeyError
 
 
 def clear() -> None:
@@ -47,4 +101,29 @@ def clear() -> None:
 
     :return: None
     """
+    global Tree, root
+    Tree.clear()
+    root = None
     return None
+
+if __name__ == '__main__':
+    insert(42, 'The meaning of life, the universe and everything.')
+    insert(0, 'ZERO!')
+    insert(13, "Devil's sign here")
+    insert(50, "blabla")
+    insert(60, "blabla")
+    insert(44, "blabla")
+    insert(55, "blabla")
+    insert(60, "blabla")
+    insert(33, "blabla")
+    insert(66, "blabla")
+    insert(51, "blabla")
+    insert(9, "blabla")
+    print(Tree.edges)
+    print(Tree.nodes)
+    Tree.remove_node(60)
+    print(Tree.edges)
+    print(Tree.nodes)
+    Tree.add_edge(42, 42)
+    nx.draw(Tree, with_labels=True)
+    plt.show()
