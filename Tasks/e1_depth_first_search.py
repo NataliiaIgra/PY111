2a480b47cb673c4b59069b87febe658d9eedbcb0
NataliiaIgra <natasha_igra@mail.ru>
2021-01-19 23:19:18 +0300

    Исправление замечания "если в графе есть кольцо".

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -18,7 +18,23 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
         already_visited.append(current_node)
 
         for node in g.neighbors(current_node):
-            if node not in already_visited:
+            if node not in already_visited and node not in stack_to_visit:
                 stack_to_visit.append(node)
 
     return already_visited
+
+
+if __name__ == '__main__':
+    graph = nx.Graph()
+    graph.add_nodes_from("ABCDEFG")
+    graph.add_edges_from([
+        ('A', 'B'),
+        ('A', 'C'),
+        ('B', 'D'),
+        ('B', 'E'),
+        ('C', 'F'),
+        ('E', 'G'),
+        ('G', 'D'),
+        ('G', 'F'),
+    ])
+    print(dfs(graph, 'A'))
