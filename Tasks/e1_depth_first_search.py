8da404ac3468ecdd1ed65027055e8cc39276fa81
NataliiaIgra <natasha_igra@mail.ru>
2021-01-19 21:42:53 +0300

    Реализация поиска в глубину, используя networkx.

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -10,5 +10,15 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    stack_to_visit = [start_node]
+    already_visited = []
+
+    while stack_to_visit:
+        current_node = stack_to_visit.pop(-1)
+        already_visited.append(current_node)
+
+        for node in g.neighbors(current_node):
+            if node not in already_visited:
+                stack_to_visit.append(node)
+
+    return already_visited
